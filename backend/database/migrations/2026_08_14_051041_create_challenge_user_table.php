f7263ae4b1b84327f14ff00497053e56e038d1d6
oguileme <guilhermemr.aluno@unipampa.edu.br>
2026-08-14 02:17:25 -0300

    feat(backend) adiciona tabela challenge_user

---
+++ backend/database/migrations/2026_08_14_051041_create_challenge_user_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('challenge_user', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('challenge_id')->constrained()->onDelete('cascade');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->boolean('completed')->default(false);
+            $table->integer('attempts')->default(0);
+            $table->integer('time_taken')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('challenge_user');
+    }
+};
