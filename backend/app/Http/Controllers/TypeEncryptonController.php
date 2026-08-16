fd22b0e6a431bd8dc8710be2e7b729e51d2355fe
oguileme <guilhermemr.aluno@unipampa.edu.br>
2026-08-16 19:33:51 -0300

    feat(backend) adiciona estrutura basica de controller e model de TypeEcrypton

---
+++ backend/app/Http/Controllers/TypeEncryptonController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\TypeEncrypton;
+use Illuminate\Http\Request;
+
+class TypeEncryptonController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+        return response()->json(TypeEncrypton::all());
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+        $data = $request->validate([
+            'name' => 'required|string|max:255',
+            'description' => 'required|string|max:255',
+        ]);
+        $typeEncrypton = TypeEncrypton::create($data);
+        return response()->json($typeEncrypton, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(TypeEncrypton $typeEncrypton)
+    {
+        //
+        return response()->json($typeEncrypton);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(TypeEncrypton $typeEncrypton)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, TypeEncrypton $typeEncrypton)
+    {
+        //
+        $data = $request->validate([
+            'name' => 'sometimes|string|max:255',
+            'description' => 'sometimes|string|max:255',
+        ]);
+        $typeEncrypton->update($data);
+        return response()->json($typeEncrypton);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(TypeEncrypton $typeEncrypton)
+    {
+        //
+        $typeEncrypton->delete();
+        return response()->json(null, 204);
+    }
+}
---
+++ backend/app/Models/TypeEncrypton.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class TypeEncrypton extends Model
+{
+    //
+    protected $table = 'type_encryption';
+
+    protected $fillable = ['name', 'description'];
+
+    
+}
