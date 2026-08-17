25ebb405f261ccddbf0dd9c33b42bf652b8f6da7
oguileme <guilhermemr.aluno@unipampa.edu.br>
2026-08-17 16:40:41 -0300

    feat(phrase) adiciona funcionalidade para pegar phrase random na model

--- backend/app/Models/Phrase.php
+++ backend/app/Models/Phrase.php
@@ -4,11 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+
 class Phrase extends Model
 {
     //
     protected $table = 'phrase';
     protected $fillable = ['phrase'];
+
+    public function getRandom(): self
+    {
+        return self::inRandomOrder()->firstOrfail();
+    }
 
     public function challenges()
     {
