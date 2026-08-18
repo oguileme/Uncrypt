395b18d64defe3286677d7a7777228d07409d01b
oguileme <guilhermemr.aluno@unipampa.edu.br>
2026-08-18 00:02:24 -0300

    fix(features) corrige estrutura de pasta + routes iniciais

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -6,16 +6,18 @@ const router = createRouter({
   routes: [
     {
       path: '/',
-      name: 'home',
+      name: 'landing',
       component: HomeView,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      path: '/login',
+      name: 'login',
+      component: () => import('../features/auth/views/LoginPage.vue'),
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: () => import('../features/auth/views/RegisterPage.vue'),
     },
   ],
 })
