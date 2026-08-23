366094b40ee7fd94e8ad1c88511d1fd1399dda46
oguileme <guilhermemr.aluno@unipampa.edu.br>
2026-08-22 23:31:58 -0300

    feat(frontend) altera typeChallege

--- frontend/src/features/challenge/types/typeChallangeUser.ts
+++ frontend/src/features/challenge/types/typeChallangeUser.ts
@@ -1,9 +1,12 @@
+import type { ChallengeType } from './typeChallenge'
+
 export interface ChallengeUserType {
   id: number
   challenge_id: number
   user_id: number
   attempts: number
-  is_completed: boolean
-  time_taken: number
+  completed: boolean
+  time_taken: number | null
   hint_used: boolean
+  challenge?: ChallengeType
 }
